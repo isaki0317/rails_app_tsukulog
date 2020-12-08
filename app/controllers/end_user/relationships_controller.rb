e5a0839fbf2411f6977a63a49de06b97720b8c03
isaki0317 <matsuoisaki.0317@gmail.com>
2020-12-08 17:14:29 +0000

    [create]チャット(非同期)管理者大枠の作成

--- app/controllers/end_user/relationships_controller.rb
+++ app/controllers/end_user/relationships_controller.rb
@@ -3,20 +3,30 @@ class EndUser::RelationshipsController < ApplicationController
 
   def create
     @end_user = EndUser.find(params[:end_user_id])
-    @user = current_end_user
     follow = current_end_user.active_relationships.build(follower_id: params[:end_user_id])
     follow.save
-    @mutual_follows = current_end_user.matchers
+    if params[:user] == "current_end_user"
+      @mutual_follows = @end_user.matchers
+    else
+      @mutual_follows = current_end_user.matchers
+      @end_user = current_end_user
+    end
     @followers = current_end_user.followers.where.not(id: current_end_user.followings)
   end
 
   def destroy
     @end_user = EndUser.find(params[:end_user_id])
-    @user = current_end_user
     follow = current_end_user.active_relationships.find_by(follower_id: params[:end_user_id])
     follow.destroy
-    @mutual_follows = current_end_user.matchers
+    if params[:user] == "end_user"
+      @mutual_follows = @end_user.matchers
+    else
+      @mutual_follows = current_end_user.matchers
+      @end_user = current_end_user
+    end
     @followers = current_end_user.followers.where.not(id: current_end_user.followings)
   end
 
 end
+
+
