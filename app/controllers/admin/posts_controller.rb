f94e1eb8b2bed5c0e1eefabba7db9848c01ff07a
isaki0317 <matsuoisaki.0317@gmail.com>
2020-12-09 08:37:07 +0000

    [crete]admin/contact

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -4,4 +4,17 @@ class Admin::PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @post = Post.find(params[:id])
+    @favorites = @post.favorites
+    @bookmarks = @post.bookmarks
+    @comments = @post.comments
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
 end
